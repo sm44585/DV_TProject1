bcd2dba9704f30c05faa94183a35ed4d6ffd97b0
Spencer Muncey <scmuncey@yahoo.com>
2015-11-05 20:11:08 -0600

    First git hub push for Project 5

--- 03 R SQL Visualizations/Crosstab.R
+++ 03 R SQL Visualizations/Crosstab.R
@@ -2,7 +2,10 @@ require(jsonlite)
 require(RCurl)
 
 # The following is equivalent to creat a crosstab with two KPIs in Tableau"
-crosstab <- vehicles %>% group_by(MAKE, YEAR) %>% summarize(sum_comb08 = sum(COMB08), sum_pv2 = sum(PV2),sum_pv4 = sum(PV4)) %>% mutate(ratio_1 = sum_comb08 / (sum_pv2))%>% mutate(ratio_2 = sum_comb08 / (sum_pv4)) %>% mutate(kpi_1 = ifelse(ratio_1 <= MPG_PV2_KPI_LOW, '03 Low', ifelse(ratio_1 <= MPG_PV2_KPI_HIGH, '02 Medium', '01 High')))%>% mutate(kpi_2 = ifelse(ratio_2 <= MPG_PV2_KPI_LOW, '03 Low', ifelse(ratio_2 <= MPG_PV2_KPI_HIGH, '02 Medium', '01 High'))) %>%filter(MAKE %in% c("Acura", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", "Chevrolet", "Dodge", "Ferrari", "Ford", "Honda", "Kia", "Lincoln", "Lexus", "Maserati", "Mazda", "Mercedes-Benz", "Nissan", "Toyota", "Volkswagen")) %>% filter(ratio_1 != Inf, ratio_2 != Inf)
+MPG_PV2_KPI_LOW = 1   
+MPG_PV2_KPI_HIGH = 2
+# The following is equivalent to creat a crosstab with two KPIs in Tableau"
+crosstab <- vehicles %>% group_by(MAKE, YEAR) %>% summarize(sum_comb08 = sum(COMB08), sum_pv2 = sum(PV2),sum_pv4 = sum(PV4)) %>% mutate(ratio_1 = sum_comb08 / (sum_pv2))%>% mutate(ratio_2 = sum_comb08 / (sum_pv4)) %>% mutate(kpi_1 = ifelse(ratio_1 < MPG_PV2_KPI_LOW, '03 Not Efficient or Spacious', ifelse(ratio_1 <= MPG_PV2_KPI_HIGH, '02 Average Efficiency and Space', '01 Efficient and Spacious')))%>% mutate(kpi_2 = ifelse(ratio_2 < MPG_PV2_KPI_LOW, '03 Not Efficient or Spacious', ifelse(ratio_2 <= MPG_PV2_KPI_HIGH, '02 Average Efficiency and Space', '01 Efficient and Spacious'))) %>%filter(MAKE %in% c("Acura", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", "Chevrolet", "Dodge", "Ferrari", "Ford", "Honda", "Kia", "Lincoln", "Lexus", "Maserati", "Mazda", "Mercedes-Benz", "Nissan", "Toyota", "Volkswagen")) %>% filter(ratio_1 != Inf, ratio_2 != Inf)
 
 # This line turns the make and year columns into ordered factors.
 crosstab <- crosstab %>% transform(MAKE = ordered(MAKE), YEAR = ordered(YEAR))
