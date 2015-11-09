7d7633753048e49057804855b25f089a747bd1a2
cc <zangcc0474@utexas.edu>
2015-11-09 17:13:50 -0600

    I think everything is done except the rmd file

--- 03 R SQL Visualizations/Blending Data.R
+++ 03 R SQL Visualizations/Blending Data.R
@@ -10,7 +10,7 @@ df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.ed
 where YEAR between 2009 and 2014
 group by YEAR
 union all
-select YERA || \\\'   Total_Vehicles_Sold\\\' as measure_names, CAR_SALE as measure_values from CAR_SALE
+select YEAR || \\\'   Total_Vehicles_Sold\\\' as measure_names, CAR_SALE as measure_values from CAR_SALE
 where YEAR between 2009 and 2014
 order by measure_names;"
 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cz4795', PASS='orcl_cz4795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(df)
